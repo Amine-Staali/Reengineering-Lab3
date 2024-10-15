6a60636ac94c1bb8a4e508d3f1341ae47ac44745
Staali Mohamed Amine <stmedamine@gmail.com>
2024-10-15 17:36:47 +0100

    LSP problem Resolved

--- LSP/src/com/directi/training/lsp/exercice_refactored/Duck.java
+++ LSP/src/com/directi/training/lsp/exercice_refactored/Duck.java
@@ -1,11 +1,15 @@
-package com.directi.training.lsp.exercise;
+package com.directi.training.lsp.exercice_refactored;
 
-public class Duck {
+public class Duck implements DuckType{
+    public Duck(){}
+
+    @Override
     public void quack()
     {
         System.out.println("Quack...");
     }
 
+    @Override
     public void swim()
     {
         System.out.println("Swim...");
