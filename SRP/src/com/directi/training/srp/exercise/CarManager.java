d2f89bcfd6da1391e014e4502e8a4375464e8afc
Staali Mohamed Amine <stmedamine@gmail.com>
2024-10-15 16:49:41 +0100

    SRP and OCP problems are resolved

--- SRP/src/com/directi/training/srp/exercise/CarManager.java
+++ SRP/src/com/directi/training/srp/exercise/CarManager.java
@@ -6,5 +6,17 @@ public class CarManager
     CarsDBManager requestFromDB;
     CarsNamesManager carsNamesManager;
     BestCarManager bestCarManager;
+
+    public Car getFromDb(final String carId){
+        return requestFromDB.getFromDb(carId);
+    }
+
+    public String getCarsNames(){
+        return carsNamesManager.getCarsNames();
+    }
+
+    public Car getBestCar() {
+        return bestCarManager.getBestCar();
+    }
 }
 
